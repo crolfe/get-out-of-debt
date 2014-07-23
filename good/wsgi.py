f934aa6498c70ed92ea4f130de5b7985d1f17e26
Colin Rolfe <crolfe@gmail.com>
2014-07-23 19:51:23 +1000

    changes to get working in production

--- good/wsgi.py
+++ good/wsgi.py
@@ -6,9 +6,10 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "good.settings")
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
