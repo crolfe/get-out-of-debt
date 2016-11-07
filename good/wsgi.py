65d621b5737d59d558e5b36246e29eff4154c2e3
Colin Rolfe <crolfe@gmail.com>
2016-11-06 17:03:34 -0800

    remove sys.path hacks

--- good/wsgi.py
+++ good/wsgi.py
@@ -7,10 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 import os
-import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
-from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'good.settings.production')
+
+from django.core.wsgi import get_wsgi_application  # NOQA
 application = get_wsgi_application()
